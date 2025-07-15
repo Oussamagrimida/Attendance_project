b6b52771cf13f79262f40aa056434565a2aa86ff
OussamaGrimida <oussamagrimida1970@gmail.com>
2025-07-15 03:04:52 +0100

    changes

--- db/conn.php
+++ db/conn.php
@@ -1,9 +1,20 @@
 <?php
-    $host='sql7.freesqldatabase.com';
+
+    // Development Connection
+     $host = '127.0.0.1';
+     $db = 'projectphp';
+     $user = 'root';
+     $pass = '';
+     $charset = 'utf8mb4';
+
+
+
+    // Remote database connection
+    /*$host='sql7.freesqldatabase.com';
     $db='sql7790035';
     $user='sql7790035';
     $pass='Gei1Xd94Aw';
-    $charset='utf8mb4';
+    $charset='utf8mb4';*/
 
     $dsn="mysql:host=$host;dbname=$db;charset=$charset";
 
@@ -14,6 +25,10 @@
         throw new PDOException($e->getMessage());
     }
 
-    require_once 'crud.php';
+     require_once 'crud.php';
+    require_once 'user.php';
     $crud = new crud($pdo);
+    $user = new user($pdo);
+   
+    $user->insertUser("admin","password");
 ?>
