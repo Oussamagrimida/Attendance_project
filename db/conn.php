de05360ea47da0b2871838d2f4df1c76c59d7984
OussamaGrimida <oussamagrimida1970@gmail.com>
2025-07-15 01:12:36 +0100

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,8 @@
 <?php
-    $host='127.0.0.1';
-    $db='projectphp';
-    $user='root';
-    $pass='';
+    $host='sql7.freesqldatabase.com';
+    $db='sql7790035';
+    $user='sql7790035';
+    $pass='Gei1Xd94Aw';
     $charset='utf8mb4';
 
     $dsn="mysql:host=$host;dbname=$db;charset=$charset";
