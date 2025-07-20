83bbf791aaa27251ccbd0eb16b7d72a51d5fb7af
OussamaGrimida <oussamagrimida1970@gmail.com>
2025-07-20 20:36:38 +0100

    fix error

--- db/conn.php
+++ db/conn.php
@@ -1,35 +1,25 @@
 <?php
+// Remote database connection
+$host='sql7.freesqldatabase.com';
+$db='sql7790035';
+$dbuser='sql7790035';
+$pass='Gei1Xd94Aw';
+$charset='utf8mb4';
 
-    // Development Connection
-     /*$host = '127.0.0.1';
-     $db = 'projectphp';
-     $dbuser = 'root';
-     $pass = '';
-     $charset = 'utf8mb4';*/
+$dsn="mysql:host=$host;dbname=$db;charset=$charset";
 
+try{
+    $pdo= new PDO($dsn,$dbuser,$pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    throw new PDOException($e->getMessage());
+}
 
+require_once 'crud.php';
+require_once 'user.php';
 
-    // Remote database connection
-    $host='sql7.freesqldatabase.com';
-    $db='sql7790035';
-    $dbuser='sql7790035';
-    $pass='Gei1Xd94Aw';
-    $charset='utf8mb4';
+$crud = new crud($pdo);
+$user = new user($pdo);
 
-    $dsn="mysql:host=$host;dbname=$db;charset=$charset";
-
-    try{
-        $pdo= new PDO($dsn,$dbuser,$pass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    }catch(PDOException $e){
-        throw new PDOException($e->getMessage());
-    }
-
-     require_once 'crud.php';
-    require_once 'user.php';
-    
-    $crud = new crud($pdo);
-    $user = new user($pdo);
-   
-    $user->insertUser("admin","password");
+$user->insertUser("admin","password");
 ?>
